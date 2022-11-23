557b4b600121d78bf9e69e2014eae8eb5ff2fcf9
Lvheng <1834805770@qq.com>
2022-11-23 20:05:52 +0800

    调整 Tooltip 示例代码

--- src/views/Tooltip.vue
+++ src/views/Tooltip.vue
@@ -1,22 +1,20 @@
 <template>
   <h2>Tooltip 提示</h2>
-  <Demo>
-    <tu-tooltip>
-      <button>我是按钮</button>
-    </tu-tooltip>
-  </Demo>
   <Demo class="tooltip-demo">
     <tu-tooltip content="Hello, I am Jack.">
       <tu-button>点击</tu-button>
     </tu-tooltip>
-  </Demo>
-  <Demo class="tooltip-demo">
     <tu-tooltip>
       <template #content>
-        <span>Hello, I am Jack.</span>
+        <span style="color: #50e2b7;">Hello, I am Jack.</span>
       </template>
       <tu-button>点击</tu-button>
     </tu-tooltip>
   </Demo>
-  <p v-for="item in 40" :key="item">你好，我的杰克。</p>
 </template>
+
+<style>
+.tooltip-demo .tu-button {
+  margin-right: 12px;
+}
+</style>
